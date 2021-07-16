f5df7ddf6362715a2102381cbdf01740ba700f2e
Noah Syrkis <noah@syrkis.com>
2021-07-16 22:35:40 +0200

    titanic pytorche exp

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -1,22 +1,36 @@
 # titanic.py
-#   baseline sollution to titanic
+#   vanilla pytorch nn titanic
 # by: Noah Syrkis
 
 # imports
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset
 import pandas as pd
-from sklearn.svm import SVC
+import numpy as np
+from icecream import ic
 
-# setup
-df = pd.read_csv('train.csv').dropna()
-y = df[df.columns[1]]
-X = df[df.columns[[2,5,6,7]]]
+# net class
+class Net(nn.Module):
+    """"""
 
-# training
-clf = SVC()
-clf.fit(X, y)
+    def __init__(self, input_dim):
+        self.fc1 = nn.Linear(input_dim, 2)
+    
+    def forward(self, x):
+        x = self.fc1(x)
+        return x
 
-# predict
-df = pd.read_csv('test.csv').dropna()
-X = df[df.columns[[1,4,5,6]]]
-p = clf.predict(X)
-print(p)
+# data read
+df = pd.read_csv('data/train.csv')
+y = torch.tensor(df['Survived'].values)
+X = df[df.columns[[2, 6, 7, 9]]]
+X = torch.tensor(np.array(X, dtype=np.float32))
+
+# train loop
+M = Net(4)
+L = nn.BCELoss()
+J = optim.Adam(params=M.parameters(), lr=0.001)
+
+for _ in range(100):
+    J.zero_grad() 
